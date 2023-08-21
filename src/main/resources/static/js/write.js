29dca4f2404059d70826f15562810ceaefb3a0ed
김광동 <test@gmail.com>
2023-08-21 18:02:05 +0900

    화면 구현

---
+++ src/main/resources/static/js/write.js
@@ -0,0 +1,53 @@
+const itemBtn = document.querySelector("#item-btn");
+const optionItems = document.querySelectorAll(".option-item");
+const itemBox = document.querySelector("#item-box");
+var editor = document.querySelector(".ql-editor");
+
+
+
+itemBtn.addEventListener("click", ()=> {
+    if(itemBox.style.height == "0px"){
+        itemBox.style. height = itemBox.scrollHeight + "px";
+    }else{
+        itemBox.style. height = "0px";
+    }
+});
+
+optionItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    itemBtn.firstElementChild.textContent = item.textContent;
+    itemBox.style. height = "0px";
+  });
+});
+
+editor.addEventListener("click", ()=>{
+    if(itemBox.style.height != "0px"){
+        itemBox.style. height = "0px";
+    }
+})
+
+
+
+
+let liv = "";
+
+optionItems.forEach((item) => {
+        item.addEventListener("click", () => {
+            liv = item.value;
+            });
+        });
+liv;
+
+function set(){
+    let content = document.querySelector("#content-mid");
+    let text = document.querySelector(".ql-editor").innerHTML;
+    let category = document.querySelector("#category");
+
+    category.value = liv;
+    content.value = text;
+}
+
+
+
+
+
